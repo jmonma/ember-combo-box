cf4736d970f6381e670482478fa484862f3e3416
Jason Monma <jason@wiredrive.com>
2014-05-28 11:28:35 -0700

    Add emberella-component-blueprint

---
+++ TODO.md
@@ -0,0 +1,11 @@
+TODOS
+==========================================
+
+* Publish to NPM/Bower
+* Add scripts/tasks for automating releases
+* Build tooling for starting a new project with the blueprint (Like Ember CLI for modules)
+* Build tooling for upgrading the blueprint features of an existing project
+* JSHint support
+* Strip console/debugger/Ember.assert (defeaturify)
+* Output code comments to documentation (YUI Doc)
+* Figure out a good way to alter project details in package.json and bower.json
---
+++ tests/support/setup.js
@@ -0,0 +1,40 @@
+document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
+
+var App = Ember.Application.create({});
+var NAMESPACE = Ember.get(ECB_OUTPUT_CONFIG.global);
+
+App.rootElement = '#ember-testing';
+
+emq.globalize();
+App.setupForTesting();
+App.injectTestHelpers();
+
+setResolver(Ember.DefaultResolver.extend({
+  testSubjects: {
+    'component:ella-sample': NAMESPACE.EllaSampleComponent
+  },
+  resolve: function(fullName) {
+    return this.testSubjects[fullName] || this._super.apply(this, arguments);
+  }
+}).create());
+
+
+Function.prototype.compile = function() {
+  var template = this.toString().split('\n').slice(1,-1).join('\n') + '\n';
+  return Ember.Handlebars.compile(template);
+};
+
+function lookupComponent(id) {
+  return Ember.View.views[id];
+}
+
+function buildComponent(test, props) {
+  props = props || {};
+  var component = test.subject(Ember.merge({
+    template: function(){/*
+      <span>Click me</span>
+    */}.compile()
+  }, props));
+  test.append();
+  return component;
+}
